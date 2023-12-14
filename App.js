19d8da1fa4bead1f2f0e78ea6740d00844003692
Gunjan Chaudhari <pc953551@gmail.com>
2023-12-14 20:32:03 +0530

    gunjan

--- App.js
+++ App.js
@@ -16,7 +16,6 @@ import Timer from './screens/components/Timer';
 // import BottomNavigator from './screens/BottomNavigator';
 import Therapy from './screens/components/Therapy';
 import Profile from './screens/components/Profile';
-import LetterDetail from './screens/LetterDetail';
 
 import QuizGame from './screens/components/QuizGame';
 import TongueTwister from './screens/components/TongueTwister';
@@ -28,12 +27,6 @@ import LipsExercise from './screens/components/LipsExercise';
 import MouthExercise from './screens/components/MouthExercise';
 import TongueExercise from './screens/components/TongueExercise';
 import Exercise from "./screens/components/Exercise";
-
-import LettersPage from "./screens/LettersPage";
-// import ButtonPage from './screens/ButtonPage';
-// import VideoPage from './screens/VideoPage';
-
-
 import LetsStart from "./screens/components/LetsStart";
 import SelfLearning2 from "./screens/components/SelfLearning2"
 import SplashScreen from './screens/components/SplashScreen';
@@ -41,16 +34,10 @@ import Activities from "./screens/components/Activities";
 import PeerInside from './screens/components/PeerInside';
 import VarnamalaPage from './screens/components/VarnamalaPage';
 import WordsPage from "./screens/components/WordsPage";
-
-import CameraScreen from "./screens/components/CameraScreen"
-import HomeScreen from './screens/HomeScreen';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-
 import CameraScreen from "./screens/components/CameraScreen";
 import LettersPage from './screens/LettersPage';
 import LetterDetail from './screens/LetterDetail';
 import HomeScreen from './screens/HomeScreen';
-
 
 
 // import MirrorTherapy from "./screens/components/MirrorTherapy";
@@ -80,20 +67,10 @@ const App = () =>{
           {/* Add other screens or stacks as needed */}
       {/* </Drawer.Navigator> */}
 
-<<<<<<< HEAD
       <Stack.Navigator initialRouteName="IntroPage">
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
-=======
-
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-
-      <Stack.Navigator>
-      <Stack.Screen name="IntroPage" component={IntroPage} />
->>>>>>> f7f2583f0148beac95e720ad07aa848f2fc68978
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-
         <Stack.Screen name="Test" component={Testing} />
         <Stack.Screen name="Games" component={Games} />
         <Stack.Screen name="PeerInside" component={PeerInside} />
@@ -101,11 +78,10 @@ const App = () =>{
         <Stack.Screen name="Balloon" component={Balloon} />
         <Stack.Screen name="IntroPage" component={IntroPage} />
         <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="CameraScreen" component={CameraScreen} />
+        <Stack.Screen name="CameraScreen" component={CameraScreen}/>
         <Stack.Screen name="Therapy" component={Therapy} />
         <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="LetterDetail" component={LetterDetail} />
-        <Stack.Screen name="LettersPage" component={LettersPage} />
+  
         <Stack.Screen name="QuizGame" component={QuizGame} />
         <Stack.Screen name="TongueTwister" component={TongueTwister} />
         <Stack.Screen name="Sentence" component={Sentence} />
@@ -122,11 +98,6 @@ const App = () =>{
         <Stack.Screen name="TongueExercise" component={TongueExercise} />
         <Stack.Screen name="MouthExercise" component={MouthExercise} />
         <Stack.Screen name="Exercise" component={Exercise} />
-
-        {/* <Stack.Screen name="ButtonPage" component={ButtonPage} /> */}
-        {/* <Stack.Screen name="VideoPage" component={VideoPage} /> */}
-
-
         <Stack.Screen name="LetsStart" component={LetsStart} />
         <Stack.Screen name="SelfLearning2" component={SelfLearning2} />
         
